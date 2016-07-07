53df4e58d087a1eb82d9256d061b0c1f88037eca
J. David Ibáñez <jdavid.ibp@gmail.com>
2016-07-07 12:01:07 +0200

    Allow spaces, split by comma

--- tagging/utils.py
+++ tagging/utils.py
@@ -25,63 +25,7 @@ def parse_tag_input(input):
         return []
 
     input = force_text(input)
-
-    # Special case - if there are no commas or double quotes in the
-    # input, we don't *do* a recall... I mean, we know we only need to
-    # split on spaces.
-    if ',' not in input and '"' not in input:
-        words = list(set(split_strip(input, ' ')))
-        words.sort()
-        return words
-
-    words = []
-    buffer = []
-    # Defer splitting of non-quoted sections until we know if there are
-    # any unquoted commas.
-    to_be_split = []
-    saw_loose_comma = False
-    open_quote = False
-    i = iter(input)
-    try:
-        while 1:
-            c = next(i)
-            if c == '"':
-                if buffer:
-                    to_be_split.append(''.join(buffer))
-                    buffer = []
-                # Find the matching quote
-                open_quote = True
-                c = next(i)
-                while c != '"':
-                    buffer.append(c)
-                    c = next(i)
-                if buffer:
-                    word = ''.join(buffer).strip()
-                    if word:
-                        words.append(word)
-                    buffer = []
-                open_quote = False
-            else:
-                if not saw_loose_comma and c == ',':
-                    saw_loose_comma = True
-                buffer.append(c)
-    except StopIteration:
-        # If we were parsing an open quote which was never closed treat
-        # the buffer as unquoted.
-        if buffer:
-            if open_quote and ',' in buffer:
-                saw_loose_comma = True
-            to_be_split.append(''.join(buffer))
-    if to_be_split:
-        if saw_loose_comma:
-            delimiter = ','
-        else:
-            delimiter = ' '
-        for chunk in to_be_split:
-            words.extend(split_strip(chunk, delimiter))
-    words = list(set(words))
-    words.sort()
-    return words
+    return list(set(split_strip(input)))
 
 
 def split_strip(input, delimiter=','):
